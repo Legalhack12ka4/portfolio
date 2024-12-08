e4b1311d2c00784ecd182607aec960488ab70b17
Vimleshkumharreformiqo <vimlesh.kumhar@reformiqo.com>
2024-12-08 15:33:26 +0530

    updated skills and profile info

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
-import Media from './Media';
+import Media from "./Media";
 
 const LeftBanner = () => {
-    const [text] = useTypewriter({
-      words: ["Professional Coder.", "Full Stack Developer.", "UI Designer."],
-      loop: true,
-      typeSpeed: 20,
-      deleteSpeed: 10,
-      delaySpeed: 2000,
-    });
+  const [text] = useTypewriter({
+    words: ["Professional Coder.", "FrontEnd React Developer.", "UI Designer."],
+    loop: true,
+    typeSpeed: 20,
+    deleteSpeed: 10,
+    delaySpeed: 2000,
+  });
   return (
     <div className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
         <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
         <h1 className="text-6xl font-bold text-white">
-          Hi, I'm <span className="text-designColor capitalize">Vimlesh Kumhar</span>
+          Hi, I'm{" "}
+          <span className="text-designColor capitalize">Vimlesh Kumhar</span>
         </h1>
         <h2 className="text-4xl font-bold text-white">
           a <span>{text}</span>
@@ -26,15 +27,19 @@ const LeftBanner = () => {
           />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wide">
-          I use animation as a third dimension by which to simplify experiences
-          and kuiding thro each and every interaction. I'm not adding motion
-          just to spruce things up, but doing it in ways that.
+          I am a skilled React Developer with over 3+ years of experience in
+          creating dynamic, user-friendly web applications. My expertise lies in
+          React, Redux, JavaScript, and modern front-end development practices.
+          I enjoy writing clean, scalable, and maintainable code and am always
+          eager to learn new technologies and frameworks to stay ahead in the
+          field. I am seeking opportunities to work with a forward-thinking team
+          and contribute to impactful projects.
         </p>
       </div>
-     {/* Media */}
-     <Media />
+      {/* Media */}
+      <Media />
     </div>
   );
-}
+};
 
-export default LeftBanner
+export default LeftBanner;
--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -1,46 +1,49 @@
-import React from 'react'
+import React from "react";
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
 import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
 const Media = () => {
   return (
     <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
-        <div>
-          <h2 className="text-base uppercase font-titleFont mb-4">
-            Find me in
-          </h2>
-          <div className="flex gap-4">
-            <span className="bannerIcon">
+      <div>
+        <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
+        <div className="flex gap-4">
+          {/* <span className="bannerIcon">
               <FaFacebookF />
             </span>
             <span className="bannerIcon">
               <FaTwitter />
-            </span>
+            </span> */}
+          <a
+            href="https://www.linkedin.com/in/vimlesh-kumhar-7b72a9217>utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+            target="_blank"
+          >
             <span className="bannerIcon">
               <FaLinkedinIn />
             </span>
-          </div>
-        </div>
-        <div>
-          <h2 className="text-base uppercase font-titleFont mb-4">
-            BEST SKILL ON
-          </h2>
-          <div className="flex gap-4">
-            <span className="bannerIcon">
-              <FaReact />
-            </span>
-            <span className="bannerIcon">
-              <SiNextdotjs />
-            </span>
-            <span className="bannerIcon">
-              <SiTailwindcss />
-            </span>
-            <span className="bannerIcon">
-              <SiFigma />
-            </span>
-          </div>
+          </a>
         </div>
       </div>
-  )
-}
+      <div>
+        <h2 className="text-base uppercase font-titleFont mb-4">
+          BEST SKILL ON
+        </h2>
+        <div className="flex gap-4">
+          <span className="bannerIcon">
+            <FaReact />
+          </span>
+          <span className="bannerIcon">
+            <SiNextdotjs />
+          </span>
+          <span className="bannerIcon">
+            <SiTailwindcss />
+          </span>
+          <span className="bannerIcon">
+            <SiFigma />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Media
+export default Media;
